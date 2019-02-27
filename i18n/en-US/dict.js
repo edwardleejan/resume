99ad279647a42a2d512372ea2a61fd43513fbaec
Aleen <aleen42@vip.qq.com>
2019-02-27 20:45:49 +0800

    init(resume): first commit

---
+++ i18n/en-US/dict.js
@@ -0,0 +1,68 @@
+module.exports = {
+    experience_type: {
+        "internship": "(internship)"，
+        "fulltime": "（fulltime）"
+    },
+    profile_type: {
+        "profile": "Profile"
+    },
+    skill_type: {
+        "video": "Computer Vision Development",
+        "desktop": "Desktop Development",
+        "frontend": "Frontend Development",
+        "backend": "Backend Development",
+        "miscellaneous": "Miscellaneous",
+        "hobbies": "Hobbies",
+        "postproduction": "Post Production",
+        "director": "Director"
+    },
+    work_type: {
+        "links": "Links"
+    },
+    about_type: {
+        "links": "Links"
+    },
+    education_type: {
+        "undergraduate": "(Bachelor's degree)",
+        "underjunior": "Junior's degree"
+    },
+    link_type: {
+        "linkedin": "LinkedIn",
+        "lofter": "Lofter",
+        "zhihu": "Zhihu",
+        "doubanbook": "Douban",
+        "repo": "Source",
+        "pdf": "PDF"
+    },
+    section_names: {
+        "education": {
+            "ch": "Education",
+            "en": "Education"
+        },
+        "language": {
+            "ch": "Language",
+            "en": "Language"
+        },
+        "profile": {
+            "ch": "Profile",
+            "en": "Profile" 
+        },
+        "experience": {
+            "ch": "Experience",
+            "en": "Experience"
+        },
+        "skills": {
+            "ch": "Skills",
+            "en": "Skills"
+        },
+        "about": {
+            "ch": "About",
+            "en": "About"
+        },
+        "works": {
+            "ch": "Works",
+            "en": "Works"
+        }
+    },
+    lt_ie8_comment: "You are using IE8 or lower version of IE, which is not supported."
+};
---
+++ i18n/zh-CN/dict.js
@@ -0,0 +1,75 @@
+module.exports = {
+    experience_type: {
+        "internship": "（实习）",
+        "fulltime": "（全职）"
+    },
+    profile_type: {
+        "profile": "自我评价"
+    },
+    skill_type: {
+        "video": "视频图像处理",
+        "desktop": "Desktop 前端",
+        "frontend": "Web 前端",
+        "backend": "Web 后端",
+        "postproduction": "后期制作",
+        "director": "编导",
+        "miscellaneous": "其他",
+        "hobbies": "爱好"
+    },
+    works_type: {
+        "links": "链接"
+    },
+    about_type: {
+        "links": "链接"
+    },
+    education_type: {
+        "undergraduate": "（本科）",
+        "underjunior": "（大专）"
+    },
+    link_type: {
+        "linkedin": "领菁",
+        "lofter": "Lofter",
+        "zhihu": "知乎",
+        "doubanbook": "豆瓣读书",
+        "repo": "源代码",
+        "pdf": "PDF 下载"
+    },
+    section_names: {
+        "education": {
+            "ch": "教育经历",
+            "en": "Education"
+        },
+        "language": {
+            "ch": "语言水平",
+            "en": "Language"
+        },
+        "profile": {
+            "ch": "个人评价",
+            "en": "Profile"
+        },
+        "experience": {
+            "ch": "项目与工作经验",
+            "en": "Experience"
+        },
+        "skills": {
+            "ch": "技能",
+            "en": "Skills"
+        },
+        "about": {
+            "ch": "相关信息",
+            "en": "About"
+        },
+        "works": {
+            "ch": "作品",
+            "en": "Works"
+        }
+    },
+    lt_ie8_comment: "你正在使用 IE8 或更老的古董浏览器，还是快升级或者换浏览器吧~",
+    dict: {
+        source_code: "源代码",
+        demo: "Demo"
+    },
+    project_type: {
+        "personal": "个人项目"
+    }
+};
